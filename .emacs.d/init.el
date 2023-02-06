3eabdd62b54d4b0cc21494157c55edc9cbce20a8
neilechao <neil.chao@lacework.net>
2023-02-06 10:03:59 -0800

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,25 @@
+;; Melpa
+(require 'package)
+ (add-to-list 'package-archives
+              '("melpa" . "https://melpa.org/packages/") t)
+ (package-initialize)
+ (package-refresh-contents)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package lsp-mode rustic go-mode rust-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'rust-mode)
+
 ;; Prompt before exiting emacs
 (defun ask-before-closing ()
   "Ask whether or not to close, and then close if y was pressed"
